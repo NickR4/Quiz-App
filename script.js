2c5a244a159726cc465a6cfefc39bb95a4998f5d
NickR4 <72036401+NickR4@users.noreply.github.com>
2021-01-07 15:47:53 -0800

    3rd update, fully functioning

--- script.js
+++ script.js
@@ -1,48 +1,22 @@
-var iQuestion = 0; 
-
-const questionsArray = [
-  {question:1, quest: "What is Portland's nickname"},
-  {question:2, quest:'who'},
-  {question:3, quest:'where'},
-  {question:4, quest:'when'},
-  {question:5, quest:'why'}
-];
-
-const answersArray = [
-[
-  {answer:1, ans: "Rose City", correct: true},
-  {answer:2, ans: "Red City", correct: false},
-  {answer:3, ans: "Rainy City", correct: false},
-  {answer:4, ans: "Riot City", correct: false}],
-
-[
-  {answer:1, ans: "A", correct: true},
-  {answer:2, ans: "B", correct: false},
-  {answer:3, ans: "C", correct: false},
-  {answer:4, ans: "D", correct: false}],
-
-[
-  {answer:1, ans: "A", correct: true},
-  {answer:2, ans: "B", correct: false},
-  {answer:3, ans: "C", correct: false},
-  {answer:4, ans: "D", correct: false}],
-
-[
-  {answer:1, ans: "A", correct: true},
-  {answer:2, ans: "B", correct: false},
-  {answer:3, ans: "C", correct: false},
-  {answer:4, ans: "D", correct: false}],
-
-[
-  {answer:1, ans: "A", correct: true},
-  {answer:2, ans: "B", correct: false},
-  {answer:3, ans: "C", correct: false},
-  {answer:4, ans: "D", correct: false}]];
 
 
+const STORE = { 
+  pages: [
+  {question:"What is Portland's nickname?", choices:['Rose City','Red City','Rainy City','Riot City'], answer:'Rose City'},
+  {question: "How did they decide to name Portland?", choices:['Raffle','Coin Toss','Vote','Naming Contest'], answer:'Coin Toss'},
+  {question: "How many bridges are there in Portland that go across the Willamette River?", choices:['6','9','12','15'], answer:'12'},
+  {question: "Portland is home of the World's Smallest _______", choices:['Horse','House','Mall','Park'], answer:'Park'},
+  {question: "Tourist Love to stop and grab a donut at this famous donut shop", choices:['Voodoo Donuts','Dunkin Donuts','Goldstar Donuts','Oregon Donuts'], answer:'Voodoo Donuts'}],
+  questionNum:0, 
+  score: 0}
+
+console.log("Curren questionNum value is "+STORE.questionNum);
+
+var questionCount = STORE.questionNum + 1
+var iQuestion = 0;
 
 function startScreen(){
-   var iQuestion = 0;
+   
   console.log("start function running");
  let startingVar=`
   <div class = "startingDiv" id = "startingDiv">
@@ -54,65 +28,113 @@ function startScreen(){
   return startingVar;
   }
 function displayStart(){
-   $('body').append(startScreen);
+   $('.central').append(startScreen);
 }
 
-
 function question (){
+  
   console.log('question activated');
-
-  var displayChoice1 = answersArray[iQuestion][0].ans;
-  var displayChoice2 = answersArray[iQuestion][1].ans;
-  var displayChoice3 = answersArray[iQuestion][2].ans;
-  var displayChoice4 = answersArray[iQuestion][3].ans;
   console.log('answers displaying');
-  console.log('current question is '+(iQuestion+1));
+
+  
   return`
-  <div class = "answerDiv"><h1>${questionsArray[iQuestion].quest}</h1>
-  <p><input type="radio" class = "radioButton"></input>${displayChoice1}</p>
-  <p><input type="radio" class = "radioButton"></input>${displayChoice2}</p>
-  <p><input type="radio" class = "radioButton"></input>${displayChoice3}</p>
-  <p><input type="radio" class = "radioButton"></input>${displayChoice4}</p>
+  <div class = "answerDiv"><h1>${STORE.pages[STORE.questionNum].question}</h1>
+  <h4>Question number ${questionCount}</h4>
+  <input type="radio" id="buttonOption" name = "radbut" value = "${STORE.pages[STORE.questionNum].choices[0]}">${STORE.pages[STORE.questionNum].choices[0]}<br>
+  <input type="radio" id="buttonOption" name = "radbut" value = "${STORE.pages[STORE.questionNum].choices[1]}">${STORE.pages[STORE.questionNum].choices[1]}<br>
+  <input type="radio" id="buttonOption" name = "radbut" value = "${STORE.pages[STORE.questionNum].choices[2]}">${STORE.pages[STORE.questionNum].choices[2]}<br>
+  <input type="radio" id="buttonOption" name = "radbut" value = "${STORE.pages[STORE.questionNum].choices[3]}">${STORE.pages[STORE.questionNum].choices[3]}<br>
+<h3 class = "scoreClass">Score ${STORE.score}</h3>
+  <h4 class = "questionCountClass">Question ${questionCount}/5</h4>
+
   <button type = "submit" class = "submitAnswer">Submit Answer</button>
   </div>`;
   
 }
 function displayQuestion(){
-  $('body').append(question);
+  console.log('displayQuestion Function Running');
+
+  $('.central').html(question);
 }
 
-function correctAnswer(){
-  if (answersArray[iQuestion][0].correct === true){console.log('CORRECT');
-  } else {console.log('FALSE');}
-    //radio button clicked, true, correct, false incorrect
-  
-  
-}
 
 function handleBeginQuizSubmit(){
   $('.startingDiv').on('click', '.startingButton', function(event) {
     console.log('start button clicked');
     displayQuestion();
-    $(handleQuestionNext)
+    $(handleQuestionSubmit)
+    $(this).remove();
   });
+  
 }
 
-function handleQuestionNext(){
-  console.log("handleQuestionNumber running");
+function handleQuestionSubmit(){
+  console.log("handleQuestionSubmit running");
   $('.answerDiv').on('click', '.submitAnswer', function(event) {
     console.log("submit answer clicked");
-    correctAnswer();
-  iQuestion++;
-  console.log("variable iQuestion at " +iQuestion);
-  displayQuestion();
+  console.log('Array Question Number is ' + STORE.questionNum);
+  checkAnswer();
+    increaseNumber();
+    displayQuestion();
+    $(handleQuestionSubmit)
+    $(this).remove();
 
-  if (iQuestion >= 5){
-    return
-    $(displayStart);
+  })};
+
+function increaseNumber(){
+  console.log('IncreaseNumber Function Activated');
+  if (STORE.questionNum < 4){
+    questionCount++;
+  STORE.questionNum++;
+  console.log('questioNum '+STORE.questionNum);}
+  else{
+    $(finalScoreDisplay)
   }
-  });}
+}
+
+function finalScore(){
+  return `
+  <div class = "restartingDiv">
+  <h1> You finished </h1>
+  <h3> Your score is ${STORE.score}</h3>
+   <button type="submit" id = "restartButton" class = "restartingButton" >Restart Quiz!</button>
+   </div>
+  `
+   
+}
+
+
+
+function handleRestart(){
+  $('.restartingDiv').on('click', '.restartingButton', function(event) {
+    console.log('restart button clicked');
+    $(handleQuestionSubmit)
+    $(this).remove();
+    STORE.questionNum = 0;
+    STORE.score = 0;
+    questionCount = 1;
+    displayQuestion();
+  });} 
+
+  function finalScoreDisplay(){
+   $('.central').html(finalScore);
+   handleRestart();
+}
+
+function checkAnswer(){
+  var answerToCheck = $('input[name="radbut"]:checked').val();
+  console.log('checking answer ' + answerToCheck);
+  
+  if (answerToCheck === STORE.pages[STORE.questionNum].answer){
+  console.log("CORRECT");
+  alert('CORRECT!');
+  STORE.score++;}
+  else{console.log("INCORRECT");
+    alert("WRONG! The correct answer is "+ STORE.pages[STORE.questionNum].answer);
+}}
 
 $(displayStart)
 $(handleBeginQuizSubmit)
+
 
 
